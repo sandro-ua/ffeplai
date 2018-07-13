8206709637024afd3daf64b1ff7249fc64de1968
sandro-ua <qatestermailbox@gmail.com>
2018-07-13 11:07:26 -0400

    13friday

---
+++ src/main/java/Fplayer.java
@@ -0,0 +1,12 @@
+public class Fplayer extends Player{
+    int normalizedRating ;
+
+
+    int calcNormalizedRating (Player player) {
+        int nRating = 0;
+        return nRating;
+    }
+
+
+
+}
